8c29647a4d7fe11eec666d2c975141538960e749
zozyo <zzy.touhou@gmail.com>
2026-08-17 22:41:15 +0800

    perf: bound classifier idle retention

--- internal/app/request/classifier.go
+++ internal/app/request/classifier.go
@@ -2,17 +2,33 @@ package request
 
 import "bytes"
 
+const maximumRetainedBodyBufferBytes int64 = 32 * 1024 * 1024
+
 func New(cfg Config) *Classifier {
 	classifier := &Classifier{cfg: cfg}
-	retainedBuffers := cfg.MaximumConcurrent
-	if retainedBuffers < 1 {
-		retainedBuffers = 1
-	}
+	retainedBuffers := retainedBodyBufferCount(cfg)
 	classifier.bodyPool = make(chan *bytes.Buffer, retainedBuffers)
 	if cfg.MaximumConcurrent > 0 {
 		classifier.tokens = make(chan struct{}, cfg.MaximumConcurrent)
 	}
 	return classifier
+}
+
+func retainedBodyBufferCount(cfg Config) int {
+	if cfg.MaximumBodyBytes <= 0 ||
+		cfg.MaximumBodyBytes > maximumRetainedBodyBufferBytes-int64(bytes.MinRead) {
+		return 0
+	}
+	maximumConcurrent := cfg.MaximumConcurrent
+	if maximumConcurrent < 1 {
+		maximumConcurrent = 1
+	}
+	perBufferBytes := cfg.MaximumBodyBytes + int64(bytes.MinRead)
+	retainedBuffers := int(maximumRetainedBodyBufferBytes / perBufferBytes)
+	if retainedBuffers > maximumConcurrent {
+		return maximumConcurrent
+	}
+	return retainedBuffers
 }
 
 func (c *Classifier) Inflight() int64 {
