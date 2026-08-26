87419175f87390b6ecd7be2293bc472c0267f3cc
zozyo <zzy.touhou@gmail.com>
2026-08-26 11:51:35 +0800

    Test TPS-only admission type ownership

---
+++ internal/admission/tps_only_surface_test.go
@@ -0,0 +1,69 @@
+package admission
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTPSOnlyAdmissionTypesDoNotOwnRetiredResourceFields(t *testing.T) {
+	tests := []struct {
+		name    string
+		typeOf  reflect.Type
+		retired []string
+	}{
+		{
+			name:   "ControllerConfig",
+			typeOf: reflect.TypeOf(ControllerConfig{}),
+			retired: []string{
+				"Capability", "WorkProfile",
+			},
+		},
+		{
+			name:   "BackendObservation",
+			typeOf: reflect.TypeOf(BackendObservation{}),
+			retired: []string{
+				"MaxModelLenTokens", "KVCapacityTokens", "KVBlockSize", "UsedKVTokens",
+				"CacheQueryTokensTotal", "CacheHitTokensTotal", "CacheCountersValid",
+			},
+		},
+		{
+			name:   "ProjectedState",
+			typeOf: reflect.TypeOf(ProjectedState{}),
+			retired: []string{
+				"ObservedKVTokens", "ReservationKVTokens", "EffectiveKVTokens",
+				"PendingPrefillInputTokens", "PendingPrefillTokens",
+				"PendingCacheCreditTokens", "PendingPrefillSequences",
+				"PendingExclusiveSequences", "PendingQuiescentSequences",
+				"LocalActiveDecode", "CacheObservationValid", "CacheHitFraction",
+				"CacheCreditFraction", "CacheEvidenceTokens", "CacheCreditBudgetTokens",
+				"CacheCreditSpentTokens",
+			},
+		},
+		{
+			name:   "DecisionRecord",
+			typeOf: reflect.TypeOf(DecisionRecord{}),
+			retired: []string{
+				"PrefillClass", "Work", "PostAdmitKVTokens", "HardKVLimitTokens",
+				"RemainingKVTokens", "PendingPrefillTokensBefore",
+				"PendingPrefillTokensAfter",
+			},
+		},
+		{
+			name:   "reservationOverlay",
+			typeOf: reflect.TypeOf(reservationOverlay{}),
+			retired: []string{
+				"kvTokens", "pendingPrefillInputTokens", "pendingPrefillTokens",
+				"pendingPrefillSequences", "pendingExclusiveSequences",
+				"pendingQuiescentSequences", "localActiveDecode",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		for _, field := range test.retired {
+			if _, exists := test.typeOf.FieldByName(field); exists {
+				t.Errorf("%s retained TPS-external field %s", test.name, field)
+			}
+		}
+	}
+}
